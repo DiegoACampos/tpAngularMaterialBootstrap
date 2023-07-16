b59fea89723977be03a1155df729d0d13ccaf934
DiegoACampos <diegoacampos.dc@gmail.com>
2023-07-16 20:23:12 -0300

    trabajo angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
-
+interface ELEMENTO {
+    nombre: string,
+    costo: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angularMAterial';
+  elementos: ELEMENTO[] = [
+    {
+      nombre: 'anana',
+      costo: '$500',
+    },
+    {
+      nombre: 'banana',
+      costo: '$600',
+    },
+    {
+      nombre: 'manzana roja',
+      costo: '$700',
+    },
+    {
+      nombre: 'manzana verde',
+      costo: '$800',
+    },
+    {
+      nombre: 'limon',
+      costo: '$900',
+    }
+  ];
+  mostrarLista:boolean = true;
+  
+  noMostrarLista(): void{
+    this.mostrarLista = !this.mostrarLista;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatTableModule} from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
